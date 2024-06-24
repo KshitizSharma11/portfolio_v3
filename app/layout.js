cceb236c4f47650907792b873f3276eba409ec57
Kshitiz Sharma <kshitizsharma.00@gmail.com>
2024-06-24 14:20:53 +0530

    bg changes and exposing secret

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         style={{
-          background: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://scontent.fjai1-3.fna.fbcdn.net/v/t39.30808-6/447681242_2312820462260381_4000141218117715_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=b32ddNDFFtUQ7kNvgE1G8OK&_nc_ht=scontent.fjai1-3.fna&oh=00_AYAgRJoeVY3_vB5jeZ-PGW9maFzeuzFgNZS0NC_Z7bbGoQ&oe=666DDFC5)`,
+          background: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(/bg_port.jpg)`,
           backgroundSize: "cover",
           backgroundPosition: "center",
 
